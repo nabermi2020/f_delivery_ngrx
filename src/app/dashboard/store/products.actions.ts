34b4ecc334af7f544a75f153485eab50085b181a
nabermi2020 <miki2017@ukr.net>
2019-10-30 22:01:27 +0200

    Action for getting product list by selected category is added

--- src/app/dashboard/store/products.actions.ts
+++ src/app/dashboard/store/products.actions.ts
@@ -3,6 +3,7 @@ import { Product } from "src/app/shared/product.model";
 
 export const GET_DEFAULT_PRODUCT_LIST = "GET_DEFAULT_PRODUCT_LIST";
 export const SAVE_PRODUCT_LIST = "SAVE_PRODUCT_LIST";
+export const GET_PRODUCT_LIST_BY_CATEGORY = "GET_PRODUCT_LIST_BY_CATEGORY";
 
 export class GetDefaultProductList implements Action {
   readonly type = GET_DEFAULT_PRODUCT_LIST;
@@ -14,6 +15,13 @@ export class SaveProductList implements Action {
   constructor(public payload: Array<Product>) {}
 }
 
+export class GetProductListByCategory implements Action {
+    readonly type = GET_PRODUCT_LIST_BY_CATEGORY;
+  
+    constructor(public payload: string) {}
+  }
+
 export type ProductsActions = 
     GetDefaultProductList |
-    SaveProductList;
+    SaveProductList | 
+    GetProductListByCategory;
